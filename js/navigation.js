4d8782a18c4ddadc6cbef3b50e994758717a94a8
dianaat <toaderandreea10@gmail.com>
2022-10-24 21:33:03 +0300

    navigation style

--- js/navigation.js
+++ js/navigation.js
@@ -4,6 +4,13 @@ function showPage(page){
     for(i=0; i<pages.length; i++) {
         if(pages[i].id !== page) {
             pages[i].style.display = "none";
+            if (document.getElementById(pages[i].id + 'Menu').classList) {
+                document.getElementById(pages[i].id + 'Menu').classList.remove('isActive');
+            } 
+        } else {
+            if(!pages[i].className.includes('isActive')) {
+                document.getElementById(page + 'Menu').className += ' isActive';
+            }
         }
     }
 }
