c198bec10dba1b43515d3a09d34bb0562b88e303
dianaat <toaderandreea10@gmail.com>
2022-10-24 21:00:31 +0300

    update site navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,9 @@
-function showAboutPage (){
-     console.log(defa);
+function showPage(page){
+    var pages = document.getElementsByClassName("pages");
+    
+    for(i=0; i<pages.length; i++) {
+        if(pages[i].id !== page) {
+            pages[i].style.display = "none";
+        }
+    }
 }
