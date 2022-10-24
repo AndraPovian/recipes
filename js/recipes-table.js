97fabc1fce778f11214ff47066776dfa941389ca
AndraPovian <109029420+AndraPovian@users.noreply.github.com>
2022-10-24 20:06:21 +0300

    Menu and site layout

--- js/recipes-table.js
+++ js/recipes-table.js
@@ -1,2 +1,11 @@
 // Functions tied to Recipes Table functionality
 // Created by Andreea
+
+function showMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
